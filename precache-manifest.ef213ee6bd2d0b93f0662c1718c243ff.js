56398f7e4558a733a3e673beb49be02926fb16e6
Fatiu Isiaka <fatiuisiaka@Fatius-MacBook-Pro.local>
2019-12-12 20:55:58 +0100

    Updates

---
+++ precache-manifest.ef213ee6bd2d0b93f0662c1718c243ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd25fe8edb097f5bdd6ab6c2cf46947a",
+    "url": "/razaqfatiu/ynfp/index.html"
+  },
+  {
+    "revision": "c30d7fee0b668f5e4b88",
+    "url": "/razaqfatiu/ynfp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "14b51da221187648ab9a",
+    "url": "/razaqfatiu/ynfp/static/css/main.c08d750b.chunk.css"
+  },
+  {
+    "revision": "c30d7fee0b668f5e4b88",
+    "url": "/razaqfatiu/ynfp/static/js/2.faaa720f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/razaqfatiu/ynfp/static/js/2.faaa720f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14b51da221187648ab9a",
+    "url": "/razaqfatiu/ynfp/static/js/main.b86be4a0.chunk.js"
+  },
+  {
+    "revision": "c1f3dc0f6e5034639a72",
+    "url": "/razaqfatiu/ynfp/static/js/runtime-main.00760bea.js"
+  }
+]);
