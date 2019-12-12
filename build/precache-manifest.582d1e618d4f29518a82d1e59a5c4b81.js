ce0b55090d8f891c69e45f20f5ce60b7901ab9d6
Fatiu Isiaka <fatiuisiaka@Fatius-MacBook-Pro.local>
2019-12-12 19:54:30 +0100

    create nav and footer

---
+++ build/precache-manifest.582d1e618d4f29518a82d1e59a5c4b81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a164f09955d9ff9b42b0ab92e1353e",
+    "url": "/ynfp/index.html"
+  },
+  {
+    "revision": "c30d7fee0b668f5e4b88",
+    "url": "/ynfp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ea03c97781650338106b",
+    "url": "/ynfp/static/css/main.c08d750b.chunk.css"
+  },
+  {
+    "revision": "c30d7fee0b668f5e4b88",
+    "url": "/ynfp/static/js/2.faaa720f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ynfp/static/js/2.faaa720f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea03c97781650338106b",
+    "url": "/ynfp/static/js/main.f1ace645.chunk.js"
+  },
+  {
+    "revision": "1ca38321bfafe8353edb",
+    "url": "/ynfp/static/js/runtime-main.6a4f5c47.js"
+  }
+]);
