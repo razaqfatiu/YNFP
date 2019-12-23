328dd0a5b8aaff34aeb358fb527dd33f3cdf9603
Fatiu Isiaka <fatiuisiaka@Fatius-MacBook-Pro.local>
2019-12-23 05:08:56 +0100

    create home page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/ynfp/">
+      <BrowserRouter>
         <div className="App">
           <Nav />
           <Switch>
