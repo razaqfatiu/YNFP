39720ed21e46b047c0973cd893416414f92a02ad
Fatiu Isiaka <fatiuisiaka@Fatius-MacBook-Pro.local>
2019-12-23 05:03:30 +0100

    Updates

---
+++ precache-manifest.04a2a18fa81489f2491edfacf858eb87.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c32dbe56a900efad23634d7d6a061a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90c27e1645db5be75fad",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e17d444356c61210e8b1",
+    "url": "/static/css/main.fb56c50e.chunk.css"
+  },
+  {
+    "revision": "90c27e1645db5be75fad",
+    "url": "/static/js/2.c979e167.chunk.js"
+  },
+  {
+    "revision": "9682237fa99bc1a358b1ac2de59fd99c",
+    "url": "/static/js/2.c979e167.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e17d444356c61210e8b1",
+    "url": "/static/js/main.97e036b8.chunk.js"
+  },
+  {
+    "revision": "c60d497a78be9ed904e2",
+    "url": "/static/js/runtime-main.3488846c.js"
+  },
+  {
+    "revision": "dfc2d7dc8d1db3ec96f6f4771925c9a4",
+    "url": "/static/media/1.dfc2d7dc.jpg"
+  },
+  {
+    "revision": "508de02f3638fdf9a10cd6b7dfd8a0a4",
+    "url": "/static/media/3.508de02f.jpg"
+  },
+  {
+    "revision": "23c6134f28c7d2078139ddff59c3942a",
+    "url": "/static/media/4.23c6134f.jpg"
+  }
+]);
